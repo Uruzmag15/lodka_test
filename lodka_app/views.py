fdf99fba7ed2384f36b8690a7843e8efd9fcef5e
Uruzmag15 <note4xacc@gmail.com>
2019-10-27 20:56:38 +0300

    Implemented POST request

--- lodka_app/views.py
+++ lodka_app/views.py
@@ -1,10 +1,36 @@
+import json
 from rest_framework import viewsets
+from rest_framework import mixins
+from django.http import HttpResponse
 
 
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(mixins.CreateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      viewsets.GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            json_data = json.loads(request.body)
+        except json.JSONDecodeError as err:
+            return HttpResponse('Incorrect json data! Error: "{}"'.format(err))
+
+        def get_or_create_object(object, parent=None):
+            obj_name = object.get('name')
+
+            if obj_name:
+                category, _ = Category.objects.get_or_create(name=obj_name,
+                                                             parent=parent)
+                for children in object.get('children', []):
+                    get_or_create_object(children, parent=category)
+
+            return
+
+        get_or_create_object(json_data)
+
+        return HttpResponse('Success!')
